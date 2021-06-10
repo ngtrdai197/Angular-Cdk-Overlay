b50517e63bfb0a6ad8d98bd52f600cd8c477523e
Dai Nguyen <ngtrdai290197@gmail.com>
2021-06-10 23:24:15 +0700

    chore(): Add prettier to format code

--- src/app/cdk-overlay.service.ts
+++ src/app/cdk-overlay.service.ts
@@ -1,91 +1,108 @@
 import { Injectable, OnDestroy, ElementRef } from '@angular/core';
-import { Overlay, OverlayRef, OverlayConfig, ConnectionPositionPair, FlexibleConnectedPositionStrategy } from '@angular/cdk/overlay';
-import { ComponentPortal, ComponentType, TemplatePortal } from '@angular/cdk/portal';
-import { ESCAPE, hasModifierKey } from '@angular/cdk/keycodes'
+import {
+	Overlay,
+	OverlayRef,
+	OverlayConfig,
+	ConnectionPositionPair,
+	FlexibleConnectedPositionStrategy,
+} from '@angular/cdk/overlay';
+import { ComponentPortal, ComponentType } from '@angular/cdk/portal';
+import { ESCAPE, hasModifierKey } from '@angular/cdk/keycodes';
 import { filter, takeUntil } from 'rxjs/operators';
 import { merge, Subject } from 'rxjs';
 import { POSITION_MAP } from './connection-position-pairs';
 
 export enum TriggerBy {
-  click = 'click',
-  hover = 'hover'
+	click = 'click',
+	hover = 'hover',
 }
 
 export enum PositionStrategy {
-  global = 'global',
-  flexible = 'flexible'
+	global = 'global',
+	flexible = 'flexible',
 }
 
-enum CurrentState {
-  opend = 'opened',
-  closed = 'closed',
+const enum CurrentState {
+	opend = 'opened',
+	closed = 'closed',
 }
 
 type ConfigCdkOverlay<T> = {
-  component: ComponentType<T>,
-  config: OverlayConfig,
-  triggerBy: keyof typeof TriggerBy,
-  position: keyof typeof PositionStrategy
-}
+	component: ComponentType<T>;
+	config: OverlayConfig;
+	triggerBy: keyof typeof TriggerBy;
+	position: keyof typeof PositionStrategy;
+};
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root',
 })
 export class CdkOverlayService implements OnDestroy {
+	public ngDestroy$ = new Subject();
+	private state: CurrentState;
+	private overlayRef: OverlayRef;
+	private positions: ConnectionPositionPair[] = [
+		POSITION_MAP.rightTop,
+		POSITION_MAP.right,
+	];
+	constructor(private overlay: Overlay) {}
 
-  public ngDestroy$ = new Subject()
-  private state: keyof typeof CurrentState
-  private overlayRef: OverlayRef
-  private portal: TemplatePortal
-  private positions: ConnectionPositionPair[] = [
-    POSITION_MAP.rightTop,
-    POSITION_MAP.right
-  ];
-  constructor(private overlay: Overlay) { }
+	ngOnDestroy() {
+		console.log(`destroyed ...`);
+		this.ngDestroy$.next();
+		this.ngDestroy$.complete();
+	}
 
-  ngOnDestroy() {
-    console.log(`destroyed ...`)
-    this.ngDestroy$.next()
-    this.ngDestroy$.complete()
-  }
+	public open<T>(overlayCdk: ConfigCdkOverlay<T>) {
+		if (this.state === CurrentState.opend) return;
+		const { component } = overlayCdk;
 
-  public open<T>(overlayCdk: ConfigCdkOverlay<T>) {
-    if (this.state === 'opend') return
-    const { component } = overlayCdk
+		const overlayRef = this.createOverlay(overlayCdk);
+		const componentPortal = new ComponentPortal(component);
+		overlayRef.attach(componentPortal);
+		this.state = CurrentState.opend;
+		this.overlayRef = overlayRef;
+		this.subscribeOverlay(overlayRef);
+	}
 
-    const overlayRef = this.createOverlay(overlayCdk)
-    const componentPortal = new ComponentPortal(component)
-    overlayRef.attach(componentPortal)
-    this.state = 'opend'
-    this.overlayRef = overlayRef
-    this.subscribeOverlay(overlayRef)
-  }
+	public close(data?: any) {
+		this.overlayRef?.detach();
+		this.state = CurrentState.closed;
+	}
 
-  public close(data?: any) {
-    this.overlayRef?.detach()
-    this.state = 'closed'
-  }
+	private createOverlay<T>(overlayCdk: ConfigCdkOverlay<T>): OverlayRef {
+		const { component, config, triggerBy, position = 'global' } = overlayCdk;
+		const positionStrategy =
+			position === 'global'
+				? this.overlay
+						.position()
+						.global()
+						.centerHorizontally()
+						.centerVertically()
+				: this.overlay
+						.position()
+						.flexibleConnectedTo(new ElementRef(component));
+		if (positionStrategy instanceof FlexibleConnectedPositionStrategy) {
+			positionStrategy.withPositions(this.positions);
+		}
+		return this.overlay.create({
+			...config,
+			hasBackdrop: triggerBy !== TriggerBy.hover,
+			positionStrategy,
+		});
+	}
 
-  private createOverlay<T>(overlayCdk: ConfigCdkOverlay<T>): OverlayRef {
-    const { component, config, triggerBy, position = 'global' } = overlayCdk
-    const positionStrategy = position === 'global' ? this.overlay.position().global().centerHorizontally().centerVertically() : this.overlay.position().flexibleConnectedTo(new ElementRef(component))
-    if (positionStrategy instanceof FlexibleConnectedPositionStrategy) {
-      positionStrategy.withPositions(this.positions)
-    }
-    return this.overlay.create({
-      ...config,
-      hasBackdrop: triggerBy !== 'hover',
-      positionStrategy
-    })
-  }
-
-  private subscribeOverlay(overlayRef: OverlayRef) {
-    merge(
-      overlayRef.backdropClick(),
-      overlayRef.detachments(),
-      overlayRef.keydownEvents().pipe(filter(event => event.keyCode === ESCAPE && !hasModifierKey(event)))
-    ).pipe(takeUntil(this.ngDestroy$)).subscribe(() => this.close('Hello world ...'))
-
-  }
-
+	private subscribeOverlay(overlayRef: OverlayRef) {
+		merge(
+			overlayRef.backdropClick(),
+			overlayRef.detachments(),
+			overlayRef
+				.keydownEvents()
+				.pipe(
+					filter(event => event.keyCode === ESCAPE && !hasModifierKey(event)),
+				),
+		)
+			.pipe(takeUntil(this.ngDestroy$))
+			.subscribe(() => this.close('Hello world ...'));
+	}
 }
--- src/app/shared/directives/cdk-overlay.directive.ts
+++ src/app/shared/directives/cdk-overlay.directive.ts
@@ -1,145 +1,169 @@
-import { Directive, Input, ViewContainerRef, HostListener, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
-import { OverlayRef, Overlay, OverlayConfig, ConnectionPositionPair, FlexibleConnectedPositionStrategy } from '@angular/cdk/overlay';
+import {
+	Directive,
+	Input,
+	ViewContainerRef,
+	HostListener,
+	ElementRef,
+	AfterViewInit,
+	OnDestroy,
+} from '@angular/core';
+import {
+	OverlayRef,
+	Overlay,
+	OverlayConfig,
+	ConnectionPositionPair,
+	FlexibleConnectedPositionStrategy,
+} from '@angular/cdk/overlay';
 import { TemplatePortal } from '@angular/cdk/portal';
 import { ESCAPE, hasModifierKey } from '@angular/cdk/keycodes';
 import { Subject, Subscription, merge } from 'rxjs';
-import { filter, tap, debounceTime } from 'rxjs/operators'
+import { filter, tap, debounceTime } from 'rxjs/operators';
 
 import { POSITION_MAP } from '../../connection-position-pairs';
 import { EventPopperComponent } from '../../event-popper/event-popper.component';
 
 const enum DialogState {
-  opened = 'opened',
-  closed = 'closed'
+	opened = 'opened',
+	closed = 'closed',
 }
 
-const enum TRIGGER_BY  {
-  click = 'click',
-  hover = 'hover'
+const enum TRIGGER_BY {
+	click = 'click',
+	hover = 'hover',
 }
 
 @Directive({
-  selector: '[appCdkOverlay]'
+	selector: '[appCdkOverlay]',
 })
 export class CdkOverlayDirective implements AfterViewInit, OnDestroy {
-  @Input() appCdkOverlay: EventPopperComponent;
-  @Input() triggerBy: TRIGGER_BY = TRIGGER_BY.click
+	@Input() appCdkOverlay: EventPopperComponent;
+	@Input() triggerBy: TRIGGER_BY = TRIGGER_BY.click;
 
-  private dialogState = DialogState.closed;
-  private click$ = new Subject<boolean>()
-  private readonly hover$ = new Subject<boolean>();
-  private portal: TemplatePortal;
-  private positions: ConnectionPositionPair[] = [
-    POSITION_MAP.leftBottomEndTop,
-    POSITION_MAP.leftBottomCenterTop,
-  ];
-  private overlayRef: OverlayRef;
-  private subscription = Subscription.EMPTY;
-  constructor(private overlay: Overlay, private viewContainerRef: ViewContainerRef, private el: ElementRef,
-  ) {
-    console.log('el', el)
-  }
+	private dialogState = DialogState.closed;
+	private click$ = new Subject<boolean>();
+	private readonly hover$ = new Subject<boolean>();
+	private portal: TemplatePortal;
+	private positions: ConnectionPositionPair[] = [
+		POSITION_MAP.leftBottomEndTop,
+		POSITION_MAP.leftBottomCenterTop,
+	];
+	private overlayRef: OverlayRef;
+	private subscription = Subscription.EMPTY;
+	constructor(
+		private overlay: Overlay,
+		private viewContainerRef: ViewContainerRef,
+		private el: ElementRef,
+	) {
+		console.log('el', el);
+	}
 
-  ngAfterViewInit() {
-    this.initialize()
-  }
+	ngAfterViewInit() {
+		this.initialize();
+	}
 
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
+	ngOnDestroy(): void {
+		this.subscription.unsubscribe();
+	}
 
-  @HostListener('click', ['$event'])
-  onClick(_event: MouseEvent) {
-    if (!this.triggerBy) {
-      return;
-    }
-    this.click$.next(true);
-  }
+	@HostListener('click', ['$event'])
+	onClick(_event: MouseEvent) {
+		if (!this.triggerBy) {
+			return;
+		}
+		this.click$.next(true);
+	}
 
-  @HostListener('mouseenter', ['$event'])
-  onMouseEnter() {
-    this.hover$.next(true);
-  }
+	@HostListener('mouseenter', ['$event'])
+	onMouseEnter() {
+		this.hover$.next(true);
+	}
 
-  @HostListener('mouseleave', ['$event'])
-  onMouseLeave() {
-    this.hover$.next(false);
-  }
+	@HostListener('mouseleave', ['$event'])
+	onMouseLeave() {
+		this.hover$.next(false);
+	}
 
-  public open() {
-    if (this.dialogState === DialogState.opened) return
-    const overlayConfig = this.getOverlayConfig()
-    this.setOverlayPosition(overlayConfig.positionStrategy as FlexibleConnectedPositionStrategy)
-    const overlayRef = this.overlay.create(overlayConfig)
+	public open() {
+		if (this.dialogState === DialogState.opened) return;
+		const overlayConfig = this.getOverlayConfig();
+		this.setOverlayPosition(
+			overlayConfig.positionStrategy as FlexibleConnectedPositionStrategy,
+		);
+		const overlayRef = this.overlay.create(overlayConfig);
 
-    overlayRef.attach(this.getPortal())
-    this.subscribeOverlayEvent(overlayRef)
-    this.overlayRef = overlayRef
-    this.dialogState = DialogState.opened
+		overlayRef.attach(this.getPortal());
+		this.subscribeOverlayEvent(overlayRef);
+		this.overlayRef = overlayRef;
+		this.dialogState = DialogState.opened;
+	}
 
-  }
+	close() {
+		if (this.dialogState === DialogState.opened) {
+			this.overlayRef?.detach();
+			this.dialogState = DialogState.closed;
+		}
+	}
 
-  close() {
-    if (this.dialogState === DialogState.opened) {
-      this.overlayRef?.detach();
-      this.dialogState = DialogState.closed;
-    }
-  }
+	private initialize() {
+		const menuVisible$ = this.appCdkOverlay.visible$;
+		const hover$ = merge(menuVisible$, this.hover$).pipe(debounceTime(100));
 
-  private initialize() {
-    const menuVisible$ = this.appCdkOverlay.visible$;
-    const hover$ = merge(menuVisible$, this.hover$).pipe(
-      debounceTime(100)
+		/*  ---true---false----
+		 *  ---------------true----false
+		 */
+		const handle$ = this.triggerBy === TRIGGER_BY.hover ? hover$ : this.click$;
+		handle$.pipe(tap(state => console.log(state))).subscribe(value => {
+			if (value) {
+				this.open();
+				return;
+			}
+			this.close();
+		});
+	}
 
-    );
+	private getOverlayConfig() {
+		const positionStrategy = this.overlay
+			.position()
+			.flexibleConnectedTo(this.el);
+		return new OverlayConfig({
+			positionStrategy: positionStrategy,
+			backdropClass: ['back-drop-modal'],
+			hasBackdrop: this.triggerBy !== TRIGGER_BY.hover,
+			scrollStrategy: this.overlay.scrollStrategies.reposition(),
+		});
+	}
 
-    /*  ---true---false----
-     *  ---------------true----false
-     */
-    const handle$ = this.triggerBy === TRIGGER_BY.hover ? hover$ : this.click$;
-    handle$.pipe(
-      tap(state => console.log(state))
-    ).subscribe(value => {
-      if (value) {
-        this.open();
-        return;
-      }
-      this.close();
-    });
-  }
+	private getPortal(): TemplatePortal {
+		if (
+			!this.portal ||
+			this.portal.templateRef !== this.appCdkOverlay.menuTemplate
+		) {
+			this.portal = new TemplatePortal<any>(
+				this.appCdkOverlay.menuTemplate,
+				this.viewContainerRef,
+			);
+		}
+		return this.portal;
+	}
 
-  private getOverlayConfig() {
-    const positionStrategy = this.overlay.position().flexibleConnectedTo(this.el)
-    return new OverlayConfig({
-      positionStrategy: positionStrategy,
-      backdropClass: ['back-drop-modal'],
-      hasBackdrop: this.triggerBy !== TRIGGER_BY.hover,
-      scrollStrategy: this.overlay.scrollStrategies.reposition()
-    })
-  }
+	private setOverlayPosition(
+		positionStrategy: FlexibleConnectedPositionStrategy,
+	) {
+		positionStrategy.withPositions([...this.positions]);
+	}
 
-  private getPortal(): TemplatePortal {
-    if (!this.portal || this.portal.templateRef !== this.appCdkOverlay.menuTemplate) {
-      this.portal = new TemplatePortal<any>(this.appCdkOverlay.menuTemplate, this.viewContainerRef);
-    }
-    return this.portal;
-  }
-
-  private setOverlayPosition(positionStrategy: FlexibleConnectedPositionStrategy) {
-    positionStrategy.withPositions([...this.positions]);
-  }
-
-  private subscribeOverlayEvent(overlayRef: OverlayRef) {
-    this.subscription.unsubscribe();
-    this.subscription = merge(
-      overlayRef.backdropClick(),
-      overlayRef.detachments(),
-      overlayRef.keydownEvents().pipe(
-        filter(event => event.keyCode === ESCAPE && !hasModifierKey(event))
-      )
-    ).subscribe(() => {
-      this.close();
-    });
-  }
-
+	private subscribeOverlayEvent(overlayRef: OverlayRef) {
+		this.subscription.unsubscribe();
+		this.subscription = merge(
+			overlayRef.backdropClick(),
+			overlayRef.detachments(),
+			overlayRef
+				.keydownEvents()
+				.pipe(
+					filter(event => event.keyCode === ESCAPE && !hasModifierKey(event)),
+				),
+		).subscribe(() => {
+			this.close();
+		});
+	}
 }
